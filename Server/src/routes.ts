8472bdf1ef1087ba630790abf3b396cd051c6b8d
Guilherme Ferreira <gui.ferreira_@hotmail.com>
2020-10-27 16:15:39 -0300

    Contrução
    Comecei a construir o Back-End do meu Login e Cadastro

--- Server/src/routes.ts
+++ Server/src/routes.ts
@@ -2,11 +2,15 @@ import express, {request, response} from'express';
 import Knex from './database/connection';
 
 import TeacherController from './controllers/TeacherController'
+import CadastroController from './controllers/CadastroController';
 
 const routs = express.Router();
 
 const teacherController = new TeacherController();
+const cadastroController = new CadastroController();
 
 routs.post('/avaliador', teacherController.create)
 routs.get('/listprof', teacherController.index)   
+routs.post('/cadastro', cadastroController.create)
+
 export default routs
