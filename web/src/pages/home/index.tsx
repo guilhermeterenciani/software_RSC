31dd5575db3c520860ecaf604e001b442434039e
Guilherme Ferreira <gui.ferreira_@hotmail.com>
2020-10-28 01:10:30 -0300

    Melhorei o Front-End do meu login

--- web/src/pages/home/index.tsx
+++ web/src/pages/home/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const CreatePoint = () => {
 
     return(
         <h1>hello,World</h1>
+        <br/><br/>
+        
+        <Link to="/login">           
+        <strong>
+         Login
+        </strong>
+        </Link>
     );
 }
 
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -3,12 +3,14 @@ import {Route, BrowserRouter} from 'react-router-dom';
 
 import Home from './pages/home';
 import CreateAvaliador from './pages/createAvaliador';
+import Login from './pages/Login';
 
 const Routes = () => {
     return(
         <BrowserRouter>
             <Route component={Home} path="/" exact/>
             <Route component={CreateAvaliador} path="/create-avaliador"/>
+            <Route component={Login} path="/login" />
         </BrowserRouter>
     )
 }
