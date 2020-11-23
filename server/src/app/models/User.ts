c845a5052b881eb1f8286abe14e741f842204fa1
Guilherme Ferreira <gui.ferreira_@hotmail.com>
2020-11-23 18:27:34 -0300

    authenticate, encriptação

--- server/src/app/models/User.ts
+++ server/src/app/models/User.ts
@@ -1,4 +1,5 @@
-import {Column, Entity, PrimaryGeneratedColumn} from 'typeorm';
+import {Column, Entity, PrimaryGeneratedColumn, BeforeInsert, BeforeUpdate} from 'typeorm';
+import bcrypt from 'bcryptjs';
 
 @Entity('users')
 class User {
